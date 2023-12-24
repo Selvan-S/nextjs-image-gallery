ecfaf6d06e9b090b005596a732e0748fe6075403
Selvan S <87435305+Selvan-S@users.noreply.github.com>
2023-12-24 12:05:15 +0530

    Nextjs-image-gallery-project

---
+++ src/app/(SSR)/users/[username]/page.tsx
@@ -0,0 +1,57 @@
+import { UnsplashUser } from "@/models/unsplash-user";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { Alert } from "@/components/bootstrap";
+interface PageProps {
+  params: { username: string };
+}
+
+async function getUser(username: string): Promise<UnsplashUser> {
+  const response = await fetch(
+    `https://api.unsplash.com/users/${username}?client_id=${process.env.UNSPLACH_ACCESS_KEY}`
+  );
+  if (response.status === 404) notFound();
+  return await response.json();
+}
+
+/**
+ * If we using other than fetch(which is native function), use cache
+ * 
+import { cache } from "react";
+ * 
+    const getUserCached = cache(getUser) // imported form react
+
+    replace getUser with getUserCached
+    eg: const user = await getUserCached(username);
+ */
+
+export async function generateMetadata({
+  params: { username },
+}: PageProps): Promise<Metadata> {
+  const user = await getUser(username);
+
+  return {
+    title:
+      ([user.first_name, user.last_name].filter(Boolean).join(" ") ||
+        user.username) + " - Image Gallery",
+  };
+}
+
+export default async function Page({ params: { username } }: PageProps) {
+  const user = await getUser(username);
+
+  return (
+    <div>
+      <Alert>
+        This profile page uses <strong>generateMetadata</strong> to set the{" "}
+        <strong>page title</strong> dynamically form the API response
+      </Alert>
+      <h1>{user.username}</h1>
+      <p>First name: {user.first_name}</p>
+      <p>Last name: {user.last_name}</p>
+      <a href={"https://unsplash.com/" + user.username} target="_blank">
+        Unsplash profile
+      </a>
+    </div>
+  );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p>Sorry, This page does not exists</p>
+    </div>
+  );
+}
